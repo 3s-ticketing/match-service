d1a69a683172cba3c3b1139baf2643aefc2de104
ParkSeYoung128 <tpdudtpdud128@gmail.com>
2026-04-22 15:48:46 +0900

    Init: initial commit

---
+++ src/main/java/org/ticketing/matchserver/MatchserverApplication.java
@@ -0,0 +1,13 @@
+package org.ticketing.matchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ticketing/matchserver/MatchserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ticketing.matchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
