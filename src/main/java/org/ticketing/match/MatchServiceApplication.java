61695d4d0e1ff7d6b121f21c42ac2a93336fea63
ParkSeYoung128 <tpdudtpdud128@gmail.com>
2026-04-27 16:32:34 +0900

    Chore: Feign 공통 설정 충돌 코드 삭제

--- src/main/java/org/ticketing/match/MatchServiceApplication.java
+++ src/main/java/org/ticketing/match/MatchServiceApplication.java
@@ -2,22 +2,13 @@ package org.ticketing.match;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
- * @EntityScan: common-module 의 Outbox 엔티티(org.ticketing.common.domain) 포함
- * @EnableJpaRepositories: common-module 의 OutboxRepository 포함
+ * @EntityScan / @EnableJpaRepositories 는 common-module 의 JPAConfig 가
+ * @EntityScan("org.ticketing") / @EnableJpaRepositories("org.ticketing") 으로 이미 적용하므로 생략한다.
+ * Feign, Kafka, JpaAuditing 등 공통 설정도 AppCtx AutoConfiguration 을 통해 자동으로 활성화된다.
  */
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "org.ticketing.match",
-        "org.ticketing.common.domain"
-})
-@EnableJpaRepositories(basePackages = {
-        "org.ticketing.match",
-        "org.ticketing.common.domain"
-})
 public class MatchServiceApplication {
 
     public static void main(String[] args) {
