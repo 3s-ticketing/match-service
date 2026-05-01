f8ae95e90c80ead95287790f215c8e49b81dd729
ParkSeYoung128 <tpdudtpdud128@gmail.com>
2026-05-01 15:34:03 +0900

    fix: match service feign client api 수정

--- src/main/java/org/ticketing/match/infrastructure/client/SeatGradeClient.java
+++ src/main/java/org/ticketing/match/infrastructure/client/SeatGradeClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "stadium-service", contextId = "seatGradeClient")
+@FeignClient(name = "seat-service", contextId = "seatGradeClient")
 public interface SeatGradeClient {
 
     @GetMapping("/api/seat-grades/{seatGradeId}/exists")
